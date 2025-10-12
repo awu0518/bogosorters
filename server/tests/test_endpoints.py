dc5376e6370a8f3a08cc71719912db5e996e85b7
Shihang Wei <wei13904639403@gmail.com>
2025-10-12 13:09:58 -0400

    add test

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -19,10 +19,12 @@ TEST_CLIENT = ep.app.test_client()
 def test_hello():
     resp = TEST_CLIENT.get(ep.HELLO_EP)
     resp_json = resp.get_json()
+    assert resp.status_code == OK
     assert ep.HELLO_RESP in resp_json
 
 def test_endpoints_lists_hello():
     resp = TEST_CLIENT.get(ep.ENDPOINT_EP)
     data = resp.get_json()
+    assert resp.status_code == OK
     assert ep.ENDPOINT_RESP in data
     assert ep.HELLO_EP in data[ep.ENDPOINT_RESP]
