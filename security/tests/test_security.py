472d08eae1e9ba23766f994c86716cf4c7b41505
NilayD-lab <nd2481@nyu.edu>
2025-12-04 22:35:16 -0500

    test_permissions

--- security/tests/test_security.py
+++ security/tests/test_security.py
@@ -7,3 +7,10 @@ def test_read():
     for feature in recs:
         assert isinstance(feature, str)
         assert len(feature) > 0
+
+
+def test_permissions():
+    # valid user
+    assert sec.has_permission("ejc369@nyu.edu", sec.PEOPLE, sec.CREATE) is True
+    # Unknown user 
+    assert sec.has_permission('sdljkf@nyu.edu', sec.PEOPLE, sec.CREATE) is False
